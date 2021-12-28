a99ae3d76eb7be770a2a49c9595edba592559f16
sk1p433 <94978240+sk1p433@users.noreply.github.com>
2021-12-28 21:10:22 +0500

    Update form.py

--- form.py
+++ form.py
@@ -1,1 +1,12 @@
 
+from django import forms
+
+from .models import Comment
+
+
+
+class UserCommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ('is_active','author','likes')
+        #widgets = {'product': forms.HiddenInput}
